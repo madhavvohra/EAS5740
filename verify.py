a318d1959e2b0074f0b068840f0cce8a42e4bd52
Madhav Vohra <44486782+madhavvohra@users.noreply.github.com>
2025-11-04 16:31:00 -0800

    Update verify.py

--- verify.py
+++ verify.py
@@ -6,39 +6,40 @@ def sign_challenge( challenge ):
 
     w3 = Web3()
 
-    sk = "0xb26f9c4751e702ac2aa81e4fd4c0581a02fcbab086755b49c97c4dc06338457a" 
+    """ To actually claim the NFT you need to write code in your own file, or use another claiming method
+    Once you have claimed an NFT you can come back to this file, update the "sk" and submit to codio to 
+    prove that you have claimed your NFT.
+    
+    This is the only line you need to modify in this file before you submit """
+    sk = "0xb26f9c4751e702ac2aa81e4fd4c0581a02fcbab086755b49c97c4dc06338457a"
 
     acct = w3.eth.account.from_key(sk)
 
-    # The 'challenge' argument is expected to be either the raw bytes or an EncodedMessage object.
-    # We remove the conversion and encoding steps here to accept the object as-is.
-    message_to_sign = challenge
-    
-    # Sign the message. This works because the object passed by the autograder is 
-    # already the required SignableMessage/EncodedMessage type.
-    signed_message = w3.eth.account.sign_message( message_to_sign , private_key = acct.key )
+    signed_message = w3.eth.account.sign_message( challenge, private_key = acct.key )
 
     return acct.address, signed_message.signature
 
 
 def verify_sig():
+    """
+        This is essentially the code that the autograder will use to test signChallenge
+        We've added it here for testing 
+    """
     
     challenge_bytes = random.randbytes(32)
 
-    # We create the EncodedMessage object once here. This covers the autograder's requirement
-    # to test the full lifecycle, passing the correct object type to sign_challenge.
-    challenge_object = encode_defunct(challenge_bytes)
-    
-    # Pass the encoded object directly to sign_challenge.
-    address, sig = sign_challenge( challenge_object ) 
+    challenge = encode_defunct(challenge_bytes)
+    address, sig = sign_challenge( challenge )
 
     w3 = Web3()
 
-    # The recover function needs the *original* EncodedMessage object.
-    return w3.eth.account.recover_message( challenge_object , signature=sig ) == address
+    return w3.eth.account.recover_message( challenge , signature=sig ) == address
 
 
 if __name__ == '__main__':
+    """
+        Test your function
+    """
     if verify_sig():
         print( f"You passed the challenge!" )
     else:
